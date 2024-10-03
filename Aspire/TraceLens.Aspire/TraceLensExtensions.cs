79c493a8fecacb9e6f558d60cf1db8f62ef967d8
Sean McGettrick <smcgettrick@gmail.com>
2024-10-03 11:35:14 -0400

    Updating Aspire hosting and PostgreSQL libraries. Updating extension method to allow specifying public ports for TraceLens and PlantUML.  Explicitly setting targetPort for WithHttpEndpoint calls.

--- Aspire/TraceLens.Aspire/TraceLensExtensions.cs
+++ Aspire/TraceLens.Aspire/TraceLensExtensions.cs
@@ -9,18 +9,25 @@ namespace TraceLens.Aspire;
 [PublicAPI]
 public static class TraceLensExtensions
 {
-    public static ContainerResource AddTraceLens(this IDistributedApplicationBuilder distributedApplicationBuilder)
+    public static ContainerResource AddTraceLens(
+        this IDistributedApplicationBuilder distributedApplicationBuilder, 
+        int? tracelensPort = 5001, 
+        int? plantUmlPort = 8080)
     {
-        var tracelensDb = distributedApplicationBuilder.AddPostgres("tracelenspgsql").AddDatabase("tracelensdb");
+        var tracelensDb = distributedApplicationBuilder
+            .AddPostgres("tracelenspgsql")
+            .AddDatabase("tracelensdb");
 
-        var plantUml = distributedApplicationBuilder.AddContainer("plantuml", "plantuml/plantuml-server", tag: "tomcat")
-            .WithHttpEndpoint(8080, name: "plantuml");
+        var plantUml = distributedApplicationBuilder
+            .AddContainer("plantuml", "plantuml/plantuml-server", tag: "tomcat")
+            .WithHttpEndpoint(port: plantUmlPort, targetPort: 8080, name: "plantuml");
 
         distributedApplicationBuilder.Services.TryAddLifecycleHook<EnvironmentVariableHook>();
         
-        var tracelens = distributedApplicationBuilder.AddContainer("tracelens", "rogeralsing/tracelens")
-            .WithHttpEndpoint(5001, name: "tracelens")
-            .WithHttpEndpoint(4317, name: "otel")
+        var tracelens = distributedApplicationBuilder
+            .AddContainer("tracelens", "rogeralsing/tracelens")
+            .WithHttpEndpoint(port: tracelensPort, targetPort: 5001, name: "tracelens")
+            .WithHttpEndpoint(port: 4317, targetPort: 4317, name: "otel")
             .WithEnvironment("PlantUml__RemoteUrl",plantUml.GetEndpoint("plantuml"))
             .WithReference(tracelensDb, "DefaultConnection");
         
